a0a8a682dc18de1d434eb784e0a80196c7352358
奕格 <yige.yyg@alibaba-inc.com>
2018-09-04 22:06:03 +0800

    Checked in by yige, separate project to part, ready to combine into webide

---
+++ src/MergeDecorationAction.ts
@@ -0,0 +1,89 @@
+import DecorationsParser from './DecorationsParser';
+
+export default function(diffEditor: any) {
+
+    let originalModel = diffEditor.getModel().original;
+    let modifiedModel = diffEditor.getModel().modified;
+    
+    let decorations: any;
+    let changes: any;
+    
+    diffEditor.onDidUpdateDiff(() => {
+      changes = diffEditor.getLineChanges();
+      let decoration = DecorationsParser.scanLineChanges(changes); 
+      // console.log('decoration', decoration);
+      decorations = originalModel.deltaDecorations(decorations, decoration);
+    });
+    
+    let lineLength, range, content, Operations;
+    
+    const mouseDown = (e: any) => {
+      let target = e.target;
+      if ( target.type === 2 ) {
+        if((target.element && target.element.previousSibling && target.element.previousSibling.classList.contains('rightArrow')) || target.element.classList.contains('rightArrow')) {
+          changes.forEach( (element: any) => {
+            if(target.position.lineNumber === element.originalStartLineNumber) {
+              content = getOriginalContent(element);
+              range = getModifiedRange(element);
+              Operations = modifiedModel.pushEditOperations([], [{
+                range: range,
+                text: content
+              }]);
+            }
+          });
+        }
+      }
+    }
+    
+    const getOriginalContent = (element: any) => {
+      let originalEndLineNumber = element.originalEndLineNumber === 0 ? element.originalStartLineNumber: element.originalEndLineNumber;
+      lineLength = originalModel.getLineLength(originalEndLineNumber) + 1;
+      range = new window.monaco.Range(element.originalStartLineNumber, 1, originalEndLineNumber, lineLength);
+      content = originalModel.getValueInRange(range);
+      // if (element.modifiedStartLineNumber === 0) {
+      //   content = content.concat('\n');
+      // } 
+      let modifiedLineCount = modifiedModel.getLineCount();
+      let originalLineCount = originalModel.getLineCount();
+      /**
+       * 如果original将要右移的行数大于modified的总行数，则需要往前补换行符
+       */
+      while(element.originalStartLineNumber - modifiedLineCount > 0) {
+        if (element.modifiedEndLineNumber === 0 && element.modifiedStartLineNumber !== 0) {
+          content = '\n'.concat(content);
+        }
+        modifiedLineCount ++;
+      }
+      /**
+       * 向左边补充换行符
+       */
+      if (element.modifiedEndLineNumber === 0 && (originalLineCount - element.originalEndLineNumber > 0 || element.modifiedStartLineNumber === 0)) {
+        content = content.concat('\n');
+      } 
+      return content;
+    }
+    
+    const getModifiedRange = (element: any) => {
+      /**
+       * element.originalEndLineNumber === 0
+       * 表示original需要整体移动到右边并且要覆盖modified对应的行
+       */
+      if(element.originalEndLineNumber === 0 && element.modifiedEndLineNumber !== 0) {
+        lineLength = modifiedModel.getLineLength(element.modifiedEndLineNumber) + 1;
+        return new window.monaco.Range(element.modifiedStartLineNumber - 1, 1, element.modifiedEndLineNumber, lineLength)
+      }
+      /**
+       * element.modifiedEndLineNumber === 0
+       * 表示original需要整体移动到右边并且要添加到对应的行的后面
+       */
+      else if(element.modifiedEndLineNumber === 0) {
+        return new window.monaco.Range(element.modifiedStartLineNumber + 1, 1, element.modifiedStartLineNumber + 1, 1)
+      } else {
+        lineLength = modifiedModel.getLineLength(element.modifiedEndLineNumber) + 1;
+        range = new window.monaco.Range(element.modifiedStartLineNumber, 1, element.modifiedEndLineNumber, lineLength);
+        return range;
+      }
+    }
+    
+    diffEditor.getOriginalEditor().onMouseDown(mouseDown);
+  }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import * as monaco from 'monaco-editor';
-import DecorationsParser from './DecorationsParser';
+import merge from './MergeDecorationAction'
 import './index.scss';
 
 self.MonacoEnvironment = {
@@ -34,91 +34,7 @@ diffEditor.setModel({
 	modified: modifiedModel
 });
 
-originalModel = diffEditor.getModel().original;
-modifiedModel = diffEditor.getModel().modified;
-
-let decorations;
-let changes;
-
-diffEditor.onDidUpdateDiff(() => {
-  changes = diffEditor.getLineChanges();
-  let decoration = DecorationsParser.scanLineChanges(changes); 
-  console.log('changes', changes);
-  // console.log('decoration', decoration);
-  decorations = originalModel.deltaDecorations(decorations, decoration);
-});
-
-let lineLength, range, content, Operations;
-
-const mouseDown = (e) => {
-  let target = e.target;
-  if ( target.type === 2 ) {
-    if((target.element && target.element.previousSibling && target.element.previousSibling.classList.contains('rightArrow')) || target.element.classList.contains('rightArrow')) {
-      changes.forEach(element => {
-        if(target.position.lineNumber === element.originalStartLineNumber) {
-          content = getOriginalContent(element);
-          range = getModifiedRange(element);
-          Operations = diffEditor.getModifiedEditor().getModel().pushEditOperations([], [{
-            range: range,
-            text: content
-          }]);
-        }
-      });
-    }
-  }
-}
-
-const getOriginalContent = (element) => {
-  let originalEndLineNumber = element.originalEndLineNumber === 0 ? element.originalStartLineNumber: element.originalEndLineNumber;
-  lineLength = diffEditor.getOriginalEditor().getModel().getLineLength(originalEndLineNumber) + 1;
-  range = new window.monaco.Range(element.originalStartLineNumber, 1, originalEndLineNumber, lineLength);
-  content = diffEditor.getOriginalEditor().getModel().getValueInRange(range);
-  // if (element.modifiedStartLineNumber === 0) {
-  //   content = content.concat('\n');
-  // } 
-  let modifiedLineCount = diffEditor.getModifiedEditor().getModel().getLineCount();
-  let originalLineCount = diffEditor.getOriginalEditor().getModel().getLineCount();
-  /**
-   * 如果original将要右移的行数大于modified的总行数，则需要往前补换行符
-   */
-  while(element.originalStartLineNumber - modifiedLineCount > 0) {
-    if (element.modifiedEndLineNumber === 0 && element.modifiedStartLineNumber !== 0) {
-      content = '\n'.concat(content);
-    }
-    modifiedLineCount ++;
-  }
-  /**
-   * 向左边补充换行符
-   */
-  if (element.modifiedEndLineNumber === 0 && (originalLineCount - element.originalEndLineNumber > 0 || element.modifiedStartLineNumber === 0)) {
-    content = content.concat('\n');
-  } 
-  return content;
-}
-
-const getModifiedRange = (element) => {
-  /**
-   * element.originalEndLineNumber === 0
-   * 表示original需要整体移动到右边并且要覆盖modified对应的行
-   */
-  if(element.originalEndLineNumber === 0 && element.modifiedEndLineNumber !== 0) {
-    lineLength = diffEditor.getModifiedEditor().getModel().getLineLength(element.modifiedEndLineNumber) + 1;
-    return new window.monaco.Range(element.modifiedStartLineNumber - 1, 1, element.modifiedEndLineNumber, lineLength)
-  }
-  /**
-   * element.modifiedEndLineNumber === 0
-   * 表示original需要整体移动到右边并且要添加到对应的行的后面
-   */
-  else if(element.modifiedEndLineNumber === 0) {
-    return new window.monaco.Range(element.modifiedStartLineNumber + 1, 1, element.modifiedStartLineNumber + 1, 1)
-  } else {
-    lineLength = diffEditor.getModifiedEditor().getModel().getLineLength(element.modifiedEndLineNumber) + 1;
-    range = new window.monaco.Range(element.modifiedStartLineNumber, 1, element.modifiedEndLineNumber, lineLength);
-    return range;
-  }
-}
-
-diffEditor.getOriginalEditor().onMouseDown(mouseDown);
+merge(diffEditor);
 
 
 /*
